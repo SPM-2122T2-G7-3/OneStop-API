b9bb4244e97009a51a3966c0c7ce47bd71ad8075
sitihindunm99 <sitihindunm.2019@sis.smu.edu.sg>
2021-10-26 23:46:32 +0800

    Completed Page for Course
    Please review.

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Course from '../views/Course.vue'
+import newQuiz from '../views/newQuiz.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/course/:courseID',
+    name: 'Course',
+    component: Course
+  },
+  {
+    path: '/course/:courseID/manageCourse/newQuiz',
+    name: 'newQuiz',
+    component: newQuiz
   }
 ]
 
