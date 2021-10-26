81af213815424737aae9f962a7cff8899d0a962c
ykloo <65279675+ykloo@users.noreply.github.com>
2021-10-26 17:27:12 +0800

    Initialize structure for frontend

---
+++ app/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <nav id="navBar" class="navbar navbar-expand-lg navbar-dark bg-primary text-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand">
+          <router-link class="nav-link text-dark font-weight-bold" to="/">All-In-One LMS</router-link> 
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              Home
+            </li>
+            <li class="nav-item">
+              Courses
+            </li>
+          </ul>
+          <div class="nav-item dropdown">
+            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              Profile
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+  }
+}
+</script>
+
+
+<style>
+#navBar {
+  padding: 20px;
+}
+
+
+</style>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <div id='header'>
+      <h1>Welcome Back</h1>
+      <h2>{{'name'}}</h2>
+      <h3>
+        What would you like to do today?
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data(){
+    return{
+      name: null,
+    }
+  },
+  methods: {
+
+  },
+  mounted: function(){
+    
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+h1 { 
+  font-size: 350%; 
+  padding-top: 30px;
+}
+
+h2 { 
+  font-size: 320%; 
+}
+
+</style>
