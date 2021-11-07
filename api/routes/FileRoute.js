d2097723b3eee517f8190d45afd55062ed9703b5
Lance F <daifa.fu.2019@scis.smu.edu.sg>
2021-11-07 16:40:18 +0800

    added route to retrieve individual material

---
+++ api/routes/FileRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const FileService = require('../services/FileService');
+
+router.get('/:filename',  (req, res, next) => {
+    const filename = req.params.filename;
+    
+    FileService.downloadFile(filename)
+        .then(readstream => {
+            res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+            readstream.pipe(res);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
+
+module.exports = router;
