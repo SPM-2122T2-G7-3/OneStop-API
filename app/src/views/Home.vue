09b31ee081b0e337517ab6a2fdedd8c871576690
ykloo <65279675+ykloo@users.noreply.github.com>
2021-11-07 21:06:53 +0800

    Updated core pages

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,10 +2,30 @@
   <div class="home">
     <div id='header'>
       <h1>Welcome Back</h1>
-      <h2>{{'name'}}</h2>
+      <h2>{{username}}</h2>
       <h3>
         What would you like to do today?
       </h3>
+    </div>
+    <div class="container">
+      <div v-if="role == 'admin'">
+        <h4 class="mt-5"><router-link to="/enrollLearners">Enroll Learners</router-link> </h4>
+        <h4 class="mt-4"><router-link to="/withdrawLearners">Withdraw Learners</router-link> </h4>
+        <h4 class="mt-4"><router-link to="/assignTrainers">Assign Trainers</router-link> </h4>
+        <h4 class="mt-4"><router-link to="/withdrawTrainers">Withdraw Trainers</router-link> </h4>
+        <h4 class="mt-4"><router-link to="/createCourse">Create Course</router-link> </h4>
+        <h4 class="mt-4"><router-link to="/createClass">Create Class</router-link> </h4>
+        <h4 class="mt-4"><router-link to="/approveSelfEnrollment">Approve Self Enrollment Applications</router-link> </h4>
+      </div>
+      <div v-else-if="role == 'trainer'">
+        <h4 class="mt-5"><router-link to="/myTeachingCourses">Manage Classes</router-link> </h4>
+      </div>
+      <div v-else>
+        <h4 class="mt-5"><router-link to="/myCourses">View currently enrolled Courses</router-link> </h4>
+        <h4 class="mt-5"><router-link to="/allCourses">View all Courses</router-link> </h4>
+        <h4 class="mt-5"><router-link to="/eligibleCourses">View Courses that I can self enroll in</router-link> </h4>
+      </div>
+
     </div>
   </div>
 </template>
@@ -21,10 +41,10 @@ export default {
     }
   },
   methods: {
-
   },
-  mounted: function(){
-    
+  created: function(){
+    this.username = this.$root.readCookie('username')
+    this.role = this.$root.readCookie('role')
   }
 }
 </script>
